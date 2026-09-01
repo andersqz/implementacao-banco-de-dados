3563e5949fda852fcef05688777afe3e8adf365a
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-09-01 19:26:24 -0300

    cast, convert e calcular idade

--- aula06/querys-aula.sql
+++ aula06/querys-aula.sql
@@ -20,7 +20,7 @@ set @idade = year(@hoje) - year(@nascimento)
 select @idade
 ------------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------------
+
 -- CAST
 select 
 	'o funcionario '
@@ -32,4 +32,31 @@ from
 ------------------------------------------------------------------------------------
 
 
+-- CONVERT SALARIO PARA STRING
+select 
+	'o funcionario '
+	+ Pnome
+	+ ' tem o salário de: R$'
+	+ convert(varchar(10), Salario) as 'nome e salario dos funcionarios'
+from
+	FUNCIONARIO
+------------------------------------------------------------------------------------
+
+
+-- CONVERT FORMATO DATA
+select 
+	'o funcionario '
+	+ Pnome
+	+ ' nasceu em '
+	+ convert(nvarchar(10), Datanasc, 109) as 'nome e nascimento dos funcionarios'
+from
+	FUNCIONARIO
+------------------------------------------------------------------------------------
+
+
+-- CONVERT formato data de nascimento usando variaveis
+declare @date date;
+set @date = (select Datanasc from FUNCIONARIO where Pnome = 'Jennifer')
+print convert(nvarchar(10), @date, 103)
+------------------------------------------------------------------------------------
 
